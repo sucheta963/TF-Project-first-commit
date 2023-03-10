db9cdab00c6f895abd7c2f450483e27fd8f02d9f
sucheta963 <suchetasharma96@yahoo.com>
2023-03-10 17:30:59 +0530

    TF project first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<>
+<nav>
+  <img src="https://www.techformation.co.in/wp-content/uploads/2018/10/Layer-0.png" alt='Tech Formation' width="200"/>
+  <ul className="nav justify-content-end">
+  <li className="nav-item">
+    <a className="nav-link active" aria-current="page" href="/">Company</a>
+  </li>
+  <li className="nav-item dropdown">
+    <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="/" role="button" aria-expanded="false">Services</a>
+    <ul className="dropdown-menu">
+      <li><a className="dropdown-item" href="/">Web Development</a></li>
+      <li><a className="dropdown-item" href="/">Website Design</a></li>
+      <li><a className="dropdown-item" href="/">Mobile Development</a></li>
+      <li><a className="dropdown-item" href="/">Website Maitainance</a></li>
+    </ul>
+  </li>
+
+  <li className="nav-item">
+    <a className="nav-link" href="/">Portfolio</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link disabled" href="/">Contact</a>
+  </li>
+<li className="nav-item">
+    <a className="nav-link disabled" href="/">Get a quote</a>
+  </li>
+</ul>
+</nav>
+</>
+  );
+}
+
+export default App;
